31153d4011292d4bb3ddb6ba1a8578e1f42316c0
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-04-07 22:47:27 +0200

    Initial commit

---
+++ lib/grocery_store_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class GroceryStoreApp extends StatelessWidget {
+  const GroceryStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Grocery Store',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("GroceryStore"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'grocery_store_app.dart';
+
+void main() {
+  runApp(const GroceryStoreApp());
+}
