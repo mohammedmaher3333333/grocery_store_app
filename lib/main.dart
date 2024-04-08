bca9937d71dad62aafbc1913a7f12183044a8fab
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-04-08 15:26:46 +0200

    firebase android setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'grocery_store_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid?
+  await Firebase.initializeApp(
+   options: const FirebaseOptions(apiKey: 'AIzaSyD5d6xvmgxf7RjppHyg8SRC_4zla9kEcwo',
+       appId: '1:252526625716:android:173588be24f3f3c041a8e1',
+       messagingSenderId: '252526625716', projectId: 'grocerystore-6a49c')
+  )
+      : await Firebase.initializeApp();
+
   runApp(const GroceryStoreApp());
 }
-//333333333333333333333333333333
