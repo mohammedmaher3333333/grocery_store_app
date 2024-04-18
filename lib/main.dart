e61ff6f20c376bcd20ad8cc1ee408f16f29ae047
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-04-18 10:56:20 +0200

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'grocery_store_app.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
