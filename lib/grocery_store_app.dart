1078df689abff8e8d366a91f221ec949d721e4ad
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-04-19 00:16:31 +0200

    route and navigator done

--- lib/grocery_store_app.dart
+++ lib/grocery_store_app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grocery_store_app/common/screens/no_network_screen.dart';
 import 'package:grocery_store_app/core/app/connectivity_controller.dart';
 import 'package:grocery_store_app/core/app/env.variables.dart';
+import 'package:grocery_store_app/routes/app_routes.dart';
 import 'package:grocery_store_app/style/fonts/font_family_helper.dart';
 import 'package:grocery_store_app/style/fonts/font_weight_helper.dart';
 
@@ -35,47 +36,8 @@ class GroceryStoreApp extends StatelessWidget {
                   ),
                 );
               },
-              home: Scaffold(
-                appBar: AppBar(
-                  centerTitle: true,
-                  title: const Text("GroceryStore"),
-                ),
-                body: Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        "old text",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text(
-                        "محمد ماهر",
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      Text(
-                        "old text",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: FontFamilyHelper.poppinsEnglish,
-                          fontWeight: FontWeightHelper.bold,
-                        ),
-                      ),
-                      Text(
-                        "محمد ماهر",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: FontFamilyHelper.cairoArabic,
-                          fontWeight: FontWeightHelper.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              onGenerateRoute: AppRoutes.onGenerateRoute,
+              initialRoute: AppRoutes.testOne,
             ),
           );
         } else {
