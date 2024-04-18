8f61f58cea4faaf0168f9adf8dc29ae334b1690b
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-04-18 18:28:04 +0200

    connectivity plus done

--- lib/grocery_store_app.dart
+++ lib/grocery_store_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_store_app/common/screens/no_network_screen.dart';
+import 'package:grocery_store_app/core/app/connectivity_controller.dart';
 import 'package:grocery_store_app/core/app/env.variables.dart';
 
 class GroceryStoreApp extends StatelessWidget {
@@ -6,19 +8,46 @@ class GroceryStoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-      title: 'Grocery Store',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("GroceryStore"),
-        ),
-      ),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, __) {
+        if (value) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            title: 'Grocery Store',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            builder: (context, widget) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return widget!;
+                  },
+                ),
+              );
+            },
+            home: Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text("GroceryStore"),
+              ),
+            ),
+          );
+        } else {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            title: 'Grocery Store Network',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
     );
   }
 }
