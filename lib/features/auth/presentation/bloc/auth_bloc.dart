30a6abef0544500441ebc7273d545511f1512f76
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-04-23 17:48:26 +0200

    sign up bloc

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -5,6 +5,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:grocery_store_app/core/service/shared_pref/pref_keys.dart';
 import 'package:grocery_store_app/core/service/shared_pref/shared_pref.dart';
 import 'package:grocery_store_app/features/auth/data/models/login_request_body.dart';
+import 'package:grocery_store_app/features/auth/data/models/sign_up_request_body.dart';
 import 'package:grocery_store_app/features/auth/data/repos/auth_repo.dart';
 
 part 'auth_event.dart';
@@ -14,7 +15,9 @@ part 'auth_bloc.freezed.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(this._repo) : super(const _Initial()) {
     on<LoginEvent>(_login);
+    on<SignUpEvent>(_signUp);
   }
+
   final AuthRepos _repo;
 
   TextEditingController emailController = TextEditingController();
@@ -23,8 +26,10 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
   final formKey = GlobalKey<FormState>();
 
+//Login
   FutureOr<void> _login(LoginEvent event, Emitter<AuthState> emit) async {
     emit(const AuthState.loading());
+
     final result = await _repo.login(
       LoginRequestBody(
         email: emailController.text.trim(),
@@ -42,13 +47,37 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         final user = await _repo.userRole(token);
         await SharedPref().setInt(PrefKeys.userId, user.userId ?? 0);
         await SharedPref().setString(PrefKeys.userRole, user.userRole ?? '');
-        // await _repo.addUserIdFirebase(userId: user.userId.toString());
+        //await _repo.addUserIdFirebase(userId: user.userId.toString());
         emit(AuthState.success(userRole: user.userRole ?? ''));
       },
       failure: (error) {
         emit(AuthState.error(error: error));
       },
     );
+  }
 
+  // signup and login to take user token
+  FutureOr<void> _signUp(
+      SignUpEvent event,
+      Emitter<AuthState> emit,
+      ) async {
+    emit(const AuthState.loading());
+    final result = await _repo.signUp(
+      SignUpRequestBody(
+        email: emailController.text.trim(),
+        password: passwordController.text,
+        avatar: event.imagUrl,
+        name: nameController.text.trim(),
+      ),
+    );
+
+    result.when(
+      success: (signupData) {
+        add(const AuthEvent.login());
+      },
+      failure: (error) {
+        emit(AuthState.error(error: error));
+      },
+    );
   }
 }
