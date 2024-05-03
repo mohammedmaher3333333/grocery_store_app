cce69c513873ab55e9d124d9d73847f44e1a274b
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-05-03 23:11:39 +0300

    dash board login in ui

--- lib/features/admin/dashboard/presentation/screens/dashboard_screen.dart
+++ lib/features/admin/dashboard/presentation/screens/dashboard_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grocery_store_app/core/common/widgets/admin_app_bar.dart';
+import 'package:grocery_store_app/core/di/injection_container.dart';
 import 'package:grocery_store_app/core/style/colors/colors_dark.dart';
+import 'package:grocery_store_app/features/admin/dashboard/presentation/bloc/categories_number/categories_number_bloc.dart';
+import 'package:grocery_store_app/features/admin/dashboard/presentation/bloc/products_number/products_number_bloc.dart';
+import 'package:grocery_store_app/features/admin/dashboard/presentation/bloc/users_number/users_number_bloc.dart';
 import 'package:grocery_store_app/features/admin/dashboard/presentation/refactors/dashboard_body.dart';
 
 class DashBoardScreen extends StatelessWidget {
@@ -8,56 +13,30 @@ class DashBoardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: ColorsDark.mainColor,
-      appBar: AdminAppBar(
-        title: 'DashBoard',
-        isMain: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<ProductsNumberBloc>()
+            ..add(const ProductsNumberEvent.getProductsNumber()),
+        ),
+        BlocProvider(
+          create: (context) => sl<CategoriesNumberBloc>()
+            ..add(const CategoriesNumberEvent.getCategoriesNumber()),
+        ),
+        BlocProvider(
+          create: (context) => sl<UsersNumberBloc>()
+            ..add(const UsersNumberEvent.getUsersNumber()),
+        ),
+      ],
+      child: const Scaffold(
         backgroundColor: ColorsDark.mainColor,
+        appBar: AdminAppBar(
+          title: 'DashBoard',
+          isMain: true,
+          backgroundColor: ColorsDark.mainColor,
+        ),
+        body: DashBoardBody(),
       ),
-        body: DashBoardBody());
-
+    );
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:grocery_store_app/core/common/widgets/admin_app_bar.dart';
-// import 'package:grocery_store_app/core/di/injection_container.dart';
-// import 'package:grocery_store_app/core/style/colors/colors_dark.dart';
-// import 'package:grocery_store_app/features/admin/dashboard/presentation/refactors/dashboard_body.dart';
-//
-// class DashBoardScreen extends StatelessWidget {
-//   const DashBoardScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiBlocProvider(
-//       providers: [
-//         BlocProvider(
-//           create: (context) => sl<ProductsNumberBloc>()
-//             ..add(const ProductsNumberEvent.getProductsNumber()),
-//         ),
-//         BlocProvider(
-//           create: (context) => sl<CategoriesNumberBloc>()
-//             ..add(const CategoriesNumberEvent.getCategoriesNumber()),
-//         ),
-//         BlocProvider(
-//           create: (context) => sl<UsersNumberBloc>()
-//             ..add(const UsersNumberEvent.getUsersNumber()),
-//         ),
-//       ],
-//       child: const Scaffold(
-//         backgroundColor: ColorsDark.mainColor,
-//         appBar: AdminAppBar(
-//           title: 'DashBoard',
-//           isMain: true,
-//           backgroundColor: ColorsDark.mainColor,
-//         ),
-//         body: DashBoardBody(),
-//       ),
-//     );
-//   }
-// }
