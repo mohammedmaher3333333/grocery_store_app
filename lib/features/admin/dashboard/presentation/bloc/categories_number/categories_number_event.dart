333d74f94ca00031d396b77b2414c585080831fb
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-05-03 20:38:34 +0300

    dashboard bloc

--- lib/features/admin/dashboard/presentation/bloc/categories_number/categories_number_event.dart
+++ lib/features/admin/dashboard/presentation/bloc/categories_number/categories_number_event.dart
@@ -1,8 +1,8 @@
-// part of 'categories_number_bloc.dart';
-//
-// @freezed
-// class CategoriesNumberEvent with _$CategoriesNumberEvent {
-//   const factory CategoriesNumberEvent.started() = _Started;
-//   const factory CategoriesNumberEvent.getCategoriesNumber() =
-//       GetCategoriesNumberEvent;
-// }
+part of 'categories_number_bloc.dart';
+
+@freezed
+class CategoriesNumberEvent with _$CategoriesNumberEvent {
+  const factory CategoriesNumberEvent.started() = _Started;
+  const factory CategoriesNumberEvent.getCategoriesNumber() =
+      GetCategoriesNumberEvent;
+}
