528dad05f8c28a4137d379c079ef9b7374a482a9
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-05-24 20:01:12 +0300

    notification hive model

--- lib/features/admin/add_notifications/presentation/screens/add_notifications_screen.dart
+++ lib/features/admin/add_notifications/presentation/screens/add_notifications_screen.dart
@@ -1,34 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_store_app/core/common/widgets/admin_app_bar.dart';
-import 'package:grocery_store_app/core/service/push_notification/firebase_cloud_messaging.dart';
 import 'package:grocery_store_app/core/style/colors/colors_dark.dart';
+import 'package:grocery_store_app/features/admin/add_notifications/presentation/refactors/add_notification_body.dart';
 
 class AddNotificationsScreen extends StatelessWidget {
   const AddNotificationsScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: ColorsDark.mainColor,
       appBar: AdminAppBar(
         title: 'Notifications',
         isMain: true,
         backgroundColor: ColorsDark.mainColor,
       ),
-      body: Center(
-        child: InkWell(
-          onTap: () async {
-            await FirebaseCloudMessaging().sendTopicNotification(
-              title: 'test Topic title',
-              body: 'test Topic body',
-              productId: -1,
-            );
-          },
-          child: Text(
-            'Notifications Screen',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-        ),
-      ),
+      body: AddNotificationBody(),
     );
   }
 }
---
+++ lib/features/admin/add_notifications/presentation/widgets/create/create_notification.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:grocery_store_app/core/common/bottom_shet/custom_bottom_sheet.dart';
+import 'package:grocery_store_app/core/common/widgets/custom_button.dart';
+import 'package:grocery_store_app/core/common/widgets/text_app.dart';
+import 'package:grocery_store_app/core/extensions/context_extension.dart';
+import 'package:grocery_store_app/core/style/colors/colors_dark.dart';
+import 'package:grocery_store_app/core/style/fonts/font_family_helper.dart';
+import 'package:grocery_store_app/core/style/fonts/font_weight_helper.dart';
+import 'package:grocery_store_app/features/admin/add_notifications/presentation/widgets/create/create_notification_bottom_sheet.dart';
+
+class CreateNotification extends StatelessWidget {
+  const CreateNotification({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        // titile
+        TextApp(
+          text: 'Notifications',
+          theme: context.textStyle.copyWith(
+            fontSize: 18.sp,
+            fontWeight: FontWeightHelper.medium,
+            fontFamily: FontFamilyHelper.poppinsEnglish,
+          ),
+        ),
+        // bottom sheet
+        CustomButton(
+          onPressed: () {
+            CustomBottomSheet.showModalBottomSheetContainer(
+              context: context,
+              widget: const CreateNotificationBottomSheet(),
+            );
+          },
+          backgroundColor: ColorsDark.blueDark,
+          lastRadius: 10,
+          threeRadius: 10,
+          text: 'Add',
+          width: 90.w,
+          height: 35.h,
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:grocery_store_app/core/common/bottom_shet/custom_bottom_sheet.dart';
+// import 'package:grocery_store_app/core/common/widgets/custom_button.dart';
+// import 'package:grocery_store_app/core/common/widgets/text_app.dart';
+// import 'package:grocery_store_app/core/extensions/context_extension.dart';
+// import 'package:grocery_store_app/core/style/colors/colors_dark.dart';
+// import 'package:grocery_store_app/core/style/fonts/font_family_helper.dart';
+// import 'package:grocery_store_app/core/style/fonts/font_weight_helper.dart';
+// import 'package:grocery_store_app/features/admin/add_notifications/presentation/widgets/create/create_notification_bottom_sheet.dart';
+//
+// class CreateNotification extends StatelessWidget {
+//   const CreateNotification({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         // titile
+//         TextApp(
+//           text: 'Notifications',
+//           theme: context.textStyle.copyWith(
+//             fontSize: 18.sp,
+//             fontWeight: FontWeightHelper.medium,
+//             fontFamily: FontFamilyHelper.poppinsEnglish,
+//           ),
+//         ),
+//         // bottom sheet
+//         CustomButton(
+//           onPressed: () {
+//             CustomBottomSheet.showModalBottomSheetContainer(
+//               context: context,
+//               widget: BlocProvider(
+//                 create: (context) => sl<AddNotificationBloc>(),
+//                 child: const CreateNotificationBottomSheet(),
+//               ),
+//               whenComplete: () {
+//                 context.read<GetAllNotificationAdminBloc>().add(
+//                       const GetAllNotificationAdminEvent.getAllNotificion(),
+//                     );
+//               },
+//             );
+//           },
+//           backgroundColor: ColorsDark.blueDark,
+//           lastRadius: 10,
+//           threeRadius: 10,
+//           text: 'Add',
+//           width: 90.w,
+//           height: 35.h,
+//         ),
+//       ],
+//     );
+//   }
+// }
